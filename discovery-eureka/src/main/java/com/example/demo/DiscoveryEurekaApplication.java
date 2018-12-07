2e856145b6ddd40d83feb2605c0be4569d14cafd
massimiliano.ortuso@capgemini.com <massimiliano.ortuso@capgemini.com>
2018-12-07 11:06:43 +0100

    add config and discovery

--- discovery-eureka/src/main/java/com/example/demo/DiscoveryEurekaApplication.java
+++ discovery-eureka/src/main/java/com/example/demo/DiscoveryEurekaApplication.java
@@ -2,13 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServerApplication {
+@EnableEurekaServer
+public class DiscoveryEurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(DiscoveryEurekaApplication.class, args);
 	}
 }
